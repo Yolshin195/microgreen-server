7ff6dbef864d9b630f341fe211cb327335a1b775
Yolshin <elshin195@gmail.com>
2021-06-02 22:30:57 +0700

    first commit

---
+++ src/main/java/ru/yolshin/microgreen/MicrogreenApplication.java
@@ -0,0 +1,13 @@
+package ru.yolshin.microgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrogreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrogreenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/yolshin/microgreen/configuration/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package ru.yolshin.microgreen.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/img/*").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+
+    }
+}
